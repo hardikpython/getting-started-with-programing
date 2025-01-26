3ff3f815888b325e22ce702decaafe78bad4f1de
hardikpython <pk646155@gmail.com>
2025-01-26 14:16:02 +0530

    print5variable

---
+++ fivediffvarible.py
@@ -0,0 +1,6 @@
+date= 25
+month= "october"
+year= 2009
+age=15
+print("birthday is on",date,month,year)
+print("now my age is", 15)
---
+++ varible.py
@@ -0,0 +1,8 @@
+Name= "hardik"
+grade=8
+section="a"
+print(Name) 
+print(grade)
+print(section)
+name = input("enter your name: ")
+print(name)
